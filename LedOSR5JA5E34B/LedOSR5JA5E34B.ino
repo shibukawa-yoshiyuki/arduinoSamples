df7ec7456e80d8d4b1fe25a008bb2a747cc9a759
shibukawa-yoshiyuki <shibukawa.yoshiyuki@atit.jp>
2024-11-15 14:53:45 +0900

    [upd]誤り修正(ピンデータ型誤り、計算式誤り)

--- LedOSR5JA5E34B/LedOSR5JA5E34B.ino
+++ LedOSR5JA5E34B/LedOSR5JA5E34B.ino
@@ -6,13 +6,13 @@
 
 using namespace ElectronicComponent;
 
-const int LED_OUT_PIN = 9;
-const int INI_LED_MIN = 22;
-const int INI_LED_MAX = 111;
+const unsigned int LED_OUT_PIN = 9U;
+const int INI_LED_MIN          = 22;
+const int INI_LED_MAX          = 111;
 
 LedOSR5JA5E34B* myLed;
 int brightVal = 0x00;
-int i         = 111;
+int i         = INI_LED_MIN;
 
 void setup() {
   myLed = new LedOSR5JA5E34B( LED_OUT_PIN );
@@ -26,7 +26,7 @@ void setup() {
 void loop() {
   brightVal = (int)round( sin( (double)i *  4.0 * M_PI / 180.0 ) * 127.0 + 127.0 );
   if( i > INI_LED_MIN ) {
-    i++;
+    i--;
   } else {
     i = INI_LED_MAX;
   }
